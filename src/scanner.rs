9a4aa5df068bb2db77d0ae46cd0e260f02de3ca8
Martin Minkov <minkovlmartin@gmail.com>
2021-11-19 20:13:10 -0800

    add scanner support for identifiers and block comments

--- src/scanner.rs
+++ src/scanner.rs
@@ -90,6 +90,14 @@ impl Scanner {
 					while self.peek() != '\n' && !self.is_at_end() {
 						self.advance();
 					}
+				} else if self.match_token('*') {
+					while !self.is_at_end() {
+						self.advance();
+						// A block comment goes until */
+						while self.peek() == '*' && self.peek_next() == '/' {
+							self.advance(); // Consume the */ part of the block comment
+						}
+					}
 				} else {
 					self.add_token(TokenType::SLASH)
 				}
@@ -100,6 +108,8 @@ impl Scanner {
 			c => {
 				if self.is_digit(c) {
 					self.number();
+				} else if self.is_alpha(c) {
+					self.identifier();
 				} else {
 					println!("Unexpected Character");
 				}
@@ -180,7 +190,42 @@ impl Scanner {
 		self.add_token(TokenType::NUMBER(num_token));
 	}
 
+	pub fn identifier(&mut self) {
+		while self.is_alpha_numeric(self.peek()) {
+			self.advance();
+		}
+
+		let identifier_token = self.source[self.start.into()..self.current.into()].to_string();
+		match &identifier_token[..] {
+			"and" => self.add_token(TokenType::AND),
+			"class" => self.add_token(TokenType::CLASS),
+			"else" => self.add_token(TokenType::ELSE),
+			"false" => self.add_token(TokenType::FALSE),
+			"for" => self.add_token(TokenType::FOR),
+			"fun" => self.add_token(TokenType::FUN),
+			"if" => self.add_token(TokenType::IF),
+			"nil" => self.add_token(TokenType::NIL),
+			"or" => self.add_token(TokenType::OR),
+			"print" => self.add_token(TokenType::PRINT),
+			"return" => self.add_token(TokenType::RETURN),
+			"super" => self.add_token(TokenType::SUPER),
+			"this" => self.add_token(TokenType::THIS),
+			"true" => self.add_token(TokenType::TRUE),
+			"var" => self.add_token(TokenType::VAR),
+			"while" => self.add_token(TokenType::WHILE),
+			_ => self.add_token(TokenType::IDENTIFIER),
+		}
+	}
+
 	pub fn is_digit(&self, c: char) -> bool {
 		c >= '0' && c <= '9'
+	}
+
+	pub fn is_alpha(&self, c: char) -> bool {
+		(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+	}
+
+	pub fn is_alpha_numeric(&self, c: char) -> bool {
+		self.is_alpha(c) || self.is_digit(c)
 	}
 }
